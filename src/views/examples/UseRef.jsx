b51194b362a0fe5f2525e1551478387de6bbeabc
GugaSantos <guga.santos.guga@gmail.com>
2023-03-15 19:02:53 -0300

    feat: using useRef with ref attribute

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -2,13 +2,31 @@ import React, { useEffect, useRef, useState } from 'react';
 import PageTitle from '../../components/layout/PageTitle';
 import SectionTitle from '../../components/layout/SectionTitle';
 
+const merge = (str1, str2) => {
+    let message = ''
+    for (const index in str1) {
+        message += str1[index] + str2[index]
+    }
+    return message
+}
+
 const UseRef = (props) => {
     const [value1, setValue1] = useState('');
+    const [value2, setValue2] = useState('');
+
     const count = useRef(0)
+    const myInput1 = useRef(null)
+    const myInput2 = useRef(null)
 
     useEffect(() => {
-        count.current = count.current + 1;
+        count.current += 1;
+        myInput2.current.focus()
     }, [value1])
+
+    useEffect(() => {
+        count.current += 1;
+        myInput1.current.focus()
+    }, [value2])
     
     return (
         <div className="UseRef">
@@ -21,15 +39,27 @@ const UseRef = (props) => {
         <div className="center">
             <div>
                 <span className='text'>Valor: </span>
-                <span className='text'>{value1} [</span>
+                <span className='text'>{merge(value1, value2)} [</span>
                 <span className='text red'>{count.current}</span>
                 <span className='text'>]</span>
             </div>
             <input 
+            ref={myInput1}
             type="text" 
             className="input"
             value={value1}
             onChange={(e) => setValue1(e.target.value)}
+             />
+        </div>
+
+        <SectionTitle title="Exercício #02"/>
+        <div className='center'>
+        <input 
+            ref={myInput2}
+            type="text" 
+            className="input"
+            value={value2}
+            onChange={(e) => setValue2(e.target.value)}
              />
         </div>
         </div>
