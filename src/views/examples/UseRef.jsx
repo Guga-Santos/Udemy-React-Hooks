8a64939bf56ad57822eddb9656fe9fdfbed4e549
GugaSantos <guga.santos.guga@gmail.com>
2023-03-15 18:48:46 -0300

    feat: using useRef with useEffect

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useEffect, useRef, useState } from 'react';
+import PageTitle from '../../components/layout/PageTitle';
+import SectionTitle from '../../components/layout/SectionTitle';
 
 const UseRef = (props) => {
+    const [value1, setValue1] = useState('');
+    const count = useRef(0)
+
+    useEffect(() => {
+        count.current = count.current + 1;
+    }, [value1])
+    
     return (
         <div className="UseRef">
             <PageTitle
                 title="Hook UseRef"
                 subtitle="Retorna um objeto mutável com a propriedade .current!"
             />
+
+        <SectionTitle title="Exercício #01"/>
+        <div className="center">
+            <div>
+                <span className='text'>Valor: </span>
+                <span className='text'>{value1} [</span>
+                <span className='text red'>{count.current}</span>
+                <span className='text'>]</span>
+            </div>
+            <input 
+            type="text" 
+            className="input"
+            value={value1}
+            onChange={(e) => setValue1(e.target.value)}
+             />
+        </div>
         </div>
     )
 }
